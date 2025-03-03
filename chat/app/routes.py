9a62be9ccd3020dedc56e827da8634bcfe41d5c5
unknown <pachenkomih@gmail.com>
2025-03-03 09:54:44 +0200

    Login/Signup page

--- chat/app/routes.py
+++ chat/app/routes.py
@@ -1,7 +1,42 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from .extensions import db
+from .models import User
 
-main = Blueprint('main', __name__)
+registration = Blueprint('registration', __name__)
 
-@main.route('/')
-def index():
-    return render_template('index.html')
+@registration.route("/", methods=["GET", "POST"])
+def login_page():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if password != None and check_password_hash(user.password, password):
+                return redirect("/pass")
+            else:
+                return render_template("login.html", username = username, password_field = True)
+        else:
+            flash("User doesn`t exists!", "error")
+            return render_template("login.html", need_signup = True)
+
+    return render_template("login.html")
+
+@registration.route("/signup", methods=["GET", "POST"])
+def signup_page():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        exists_user = User.query.filter_by(username=username).first()
+        if exists_user:
+            flash("User already exists!", "error")
+            return render_template("signup.html")
+        else:
+            new_user = User(username=username, password=generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect("/pass")
+
+    return render_template("signup.html")
